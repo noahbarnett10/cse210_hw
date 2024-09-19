28437674a3ef93919adf4e68a2c1e9c12d1a1a9a
NBarnett <noah.barnett10@gmail.com>
2024-09-19 10:36:01 -0600

    Prep2 assignment completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userPercentage = Console.ReadLine();
+        int percentage = int.Parse(userPercentage);
+        string letter;
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"You have a {letter} for this course.");
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You have passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You have not passed. Better luck next time!");
+        }
     }
 }
