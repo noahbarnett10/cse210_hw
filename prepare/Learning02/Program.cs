8527a765d8d01eb4f7b9e285d336b15038495c5d
NBarnett <noah.barnett10@gmail.com>
2024-10-02 18:20:42 -0600

    W03 Task Completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Electrical Engineer";
+        job1._startYear = 2020;
+        job1._endYear = 2024;
+        // job1.Display();
+
+        Job job2 = new Job();
+        job2._company = "Intel";
+        job2._jobTitle = "Software Engineer";
+        job2._startYear = 2014;
+        job2._endYear = 2020;
+        // job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Noah Barnett";
+        myResume._jobs.Add(job1); 
+        myResume._jobs.Add(job2); 
+        myResume.Display();
+
     }
 }
