557023286f100d9761411c3e51139a375e768855
NBarnett <noah.barnett10@gmail.com>
2024-10-11 16:56:21 -0600

    Develop 2 completed

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,6 +1,103 @@
 using System;
+using System.Xml.Serialization;
+using System.IO;
+using System.Runtime.InteropServices;
 
-partial class Program
+class PromptGenerator 
+{
+    public static string PromptFun(string[] args)
+    {
+        string[] _prompts = new string[5];
+        _prompts[0] = "Who did I have the best conversation with today?";
+        _prompts[1] = "What was the most notable part of my day today?";
+        _prompts[2] = "What are some blessings I noticed today?";
+        _prompts[3] = "What emotions did I feel today?";
+        _prompts[4] = "If I had one thing I could do over today, what would it be?";
+        Random rnd = new Random();
+        // Console.WriteLine(_prompts[rnd.Next(0,5)]);
+        string randomPrompt = (_prompts[rnd.Next(0,5)]);
+
+        return randomPrompt;
+    }
+}
+
+class Entry 
+{
+    public static void JournalEntry(string randomPrompt)
+    {
+        string filename = "myFile.txt";
+        using (StreamWriter outputFile = new StreamWriter(filename, append: true))
+        {
+        outputFile.WriteLine($"\nDate: {DateTime.Now.ToString("yyyy-MM-dd")}");
+        Console.WriteLine($"\nDate: {DateTime.Now.ToString("yyyy-MM-dd")}");
+        // PromptGenerator.PromptFun(new string[0]); 
+        outputFile.WriteLine($"Prompt: {randomPrompt}");
+        Console.WriteLine($"Prompt: {randomPrompt}");
+        outputFile.Write("> ");
+        Console.Write("> ");
+        string Entry = Console.ReadLine();
+        outputFile.WriteLine(Entry);
+        }
+    }
+}
+
+class Journal
+{
+    public static void DisplayJournal()
+    {
+        string filename = "myFile.txt";
+        string[] lines = File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+    }
+
+    public static string[] LoadJournal()
+    {
+        Console.Write("What is the file name: ");
+        string filename = Console.ReadLine();
+        string[] fileContents;
+        if (File.Exists(filename))
+        {
+            fileContents = File.ReadAllLines(filename);
+            string tempFileName = "myFile.txt";
+            using (StreamWriter tempFile = new StreamWriter(tempFileName, append: false))
+            {
+            foreach (string line in fileContents)
+            {
+                tempFile.WriteLine(line);
+            }
+            }
+
+        }
+        else 
+        {
+            Console.WriteLine("That file does not exist.");
+            fileContents = [];
+        }
+        return fileContents;
+    }
+
+    public static void SaveJournal()
+    {
+        Console.Write("What is the file to save to? ");
+        string filename = Console.ReadLine();
+        using (StreamWriter outputFile = new StreamWriter(filename, append: false))
+        {
+            string txtFile = "myFile.txt";
+            string[] lines = File.ReadAllLines(txtFile);
+            foreach (string line in lines)
+            {
+                outputFile.WriteLine(line);
+            }
+        }
+
+    }
+}
+
+class Program
 {
     static void Main(string[] args)
     {
@@ -17,37 +114,31 @@ partial class Program
             Console.Write("What would you like to do? ");
             userResponse = int.Parse(Console.ReadLine());
 
-            if (userResponse == 1) {
-                Write writePrompt = new Write();
-                writePrompt.promptFun(args); // Calling the Write.cs 
-                // string fileName = "myJournal.txt";
-                // using (StreamWriter outputFile = new StreamWriter(fileName))
-                // {
-                //     outputFile.WriteLine();
-                // }
-
+            if (userResponse == 1) 
+            {
+                string randomPrompt = PromptGenerator.PromptFun(args);
+                Entry.JournalEntry(randomPrompt);
             }
 
-            else if (userResponse == 2) {
-                string fileName = "myJournal.txt";
-                string [] lines = System.IO.File.ReadAllLines(fileName);
-
-                foreach(string line in lines) {
-                    Console.WriteLine(line);
-                }
+            else if (userResponse == 2) 
+            {
+                Journal.DisplayJournal();
             }
 
-            else if (userResponse == 3) {
-               
-
+            else if (userResponse == 3) 
+            {
+               Journal.LoadJournal();
             }
 
-            else if (userResponse == 4) {
-            Save saved = new Save();
-                saved.saveFun();
-
+            else if (userResponse == 4) 
+            {
+                Journal.SaveJournal();
             }
         }
-
+    string filename = "myFile.txt";
+    using (StreamWriter outputFile = new StreamWriter(filename, append: false))
+    {
+        Console.WriteLine("");
+    }
     }
 }
