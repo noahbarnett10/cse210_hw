d2a352950c1758433226787acd3ceb86d74e1f9a
NBarnett <noah.barnett10@gmail.com>
2024-10-30 16:26:54 -0600

    Learning Activity Completed

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment A = new Assignment("Noah Barnett", "Circuits");
+        Console.WriteLine(A.GetSummary());
+
+        MathAssignment MA = new MathAssignment("Noah Barnett", "Circuits", "6.5", "11-14");
+        Console.WriteLine(MA.GetHomeworkList());
+
+        WritingAssignment WA = new WritingAssignment("Noah Barnett", "Circuits", "Digital Systems");
+        Console.WriteLine(WA.GetWritingInformation());
     }
 }
